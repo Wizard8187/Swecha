b8df0ed0d839edd13753722150f89aca32aac057
Wizard8187 <144228054+Wizard8187@users.noreply.github.com>
2023-09-16 02:49:28 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form');
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        // Create a new list item to display the response
-        const listItem = document.createElement('li');
+    const listItem = document.createElement('li');
         listItem.innerText = `Q: ${form.question.value}, A: ${form.answer.value}`;
-        // Clear the form
-        form.reset();
-    });
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault(); //
+              
+                const formData = new FormData(form);
+                const postData = {};
+              
+                formData.forEach((value, key) => {
+                  postData[key] = value;
+                });
+              
+                try {
+                    //Your API Link goes here
+                  const response = await fetch('https://your-strapi-api.com/api/your-content-type', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(postData),
+                  });
+              
+                  if (response.ok) {
+                    console.log('Data submitted successfully');
+                    form.reset(); 
+                  } else {
+                    
+                    console.error('Error submitting data');
+                  }
+                } catch (error) {
+                  
+                  console.error('Network error:', error);
+                }
+              });
+                });
     });
